d1eb09aacc9dfdf29532ec97bc7556e42673311c
Scott Feichter <scottfeichter@gmail.com>
2024-01-31 10:05:22 -0800

    bind practice

--- week-4/d3-context-bind-call-apply/practices/02-bind/test.js
+++ week-4/d3-context-bind-call-apply/practices/02-bind/test.js
@@ -1,1 +1,11 @@
-// Your code here
+const Employee = require(`./employee`);
+
+const johnWick = new Employee("John Wick", "Dog Lover");
+
+setTimeout(johnWick.sayName, 2000); // we lose context
+
+let sayNameBound = johnWick.sayName.bind(johnWick);
+
+setTimeout(sayNameBound, 1000); // we keep context
+
+setTimeout(johnWick.sayOccupation.bind(johnWick), 500); // also keep context
