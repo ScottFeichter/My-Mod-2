67aa51416ddba08fed1901bc241c9112458d2c08
Scott Feichter <scottfeichter@gmail.com>
2024-02-07 08:48:38 -0800

    linked-list

--- week-5/d3-linked-lists-and-queues/practices/01-linked-lists/linked-list.js
+++ week-5/d3-linked-lists-and-queues/practices/01-linked-lists/linked-list.js
@@ -7,11 +7,13 @@ class LinkedListNode {
 
 class LinkedList {
   constructor() {
-    // Your code here
+    this.head = null;
   }
 
   addToHead(val) {
-    // Your code here
+    const newNode = new LinkedListNode(value, null); // create new node
+    newNode.next = this.head; // set next to current head
+    this.head = newNode; // set head to point to the new link
   }
 
   addToTail(val) {
@@ -20,14 +22,14 @@ class LinkedList {
 
   // You can use this function to help debug
   print() {
-    let current = this.head;
+    let current = this.head; // grab the head
 
     while (current) {
-      process.stdout.write(`${current.value} -> `);
-      current = current.next;
+      process.stdout.write(`${current.value} -> `); // print value
+      current = current.next; // assign current to next node
     }
 
-    console.log("NULL");
+    console.log("NULL"); // we indicate we are at end
   }
 }
 
