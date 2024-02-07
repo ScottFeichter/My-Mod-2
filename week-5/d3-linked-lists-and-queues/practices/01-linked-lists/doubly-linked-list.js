e8664bbca7e9caac94193ca483b20ad1a57275e1
Scott Feichter <scottfeichter@gmail.com>
2024-02-07 12:31:38 -0800

    updating

--- week-5/d3-linked-lists-and-queues/practices/01-linked-lists/doubly-linked-list.js
+++ week-5/d3-linked-lists-and-queues/practices/01-linked-lists/doubly-linked-list.js
@@ -1,3 +1,32 @@
+/*
+
+add to head:
+1. length++
+2. init nuNode
+3. if (!this.head)
+  I. no head (and no tail)
+   a. this.head = nuNode
+   b. this.tail = nuNode
+  II. yes head (and yes tail)
+   a. this.head.prev = nuNode
+   b. nuNode.next = this.head
+   c. this.head = nuNode
+
+
+add to tail:
+1. length++
+2. init nuNode
+3. if (!this.tail)
+  I. no tail (and no head)
+   a. this.head = nuNode
+   b. this.tail = nuNode
+  II. yes tail (and yes head)
+   a. this.tail.next = nuNode
+   b. nuNode.prev = this.tail
+   c. this.tail = nuNode
+
+*/
+
 class DoublyLinkedListNode {
   constructor(val) {
     this.value = val;
@@ -8,26 +37,47 @@ class DoublyLinkedListNode {
 
 class DoublyLinkedList {
   constructor() {
-    // Your code here
+    this.head = null;
+    this.tail = null;
+    this.length = 0;
   }
 
   addToHead(val) {
-    // Your code here
+    this.length++;
+    const nuNode = new DoublyLinkedListNode(val);
+    if (!this.head) {
+      this.head = nuNode; // empty list so nuNode becomes head and tail
+      this.tail = nuNode;
+      return;
+    } else {
+      this.head.prev = nuNode; // bc we are about to shove nuNode in front of it
+      nuNode.next = this.head; // bc we are about to shove it to next position
+      this.head = nuNode; // we shove nuNode in head position
+      return;
+    }
   }
 
   addToTail(val) {
-    // Your code here
+    this.length++;
+    const nuNode = new DoublyLinkedListNode(val);
+
+    if (!this.tail) {
+      this.tail = nuNode;
+      this.head = nuNode;
+    } else {
+      this.tail.next = nuNode; // bc we are about to shove nuNode after tail
+      nuNode.prev = this.tail; // bc we are about to have nuNode after tail
+      this.tail = nuNode; // nuNode becomes tail
+    }
   }
 
   // You can use this function to help debug
   print() {
     let current = this.head;
-
     while (current) {
       process.stdout.write(`${current.value} <-> `);
       current = current.next;
     }
-
     console.log("NULL");
   }
 }
--- week-5/d3-linked-lists-and-queues/practices/01-linked-lists/linked-list.js
+++ week-5/d3-linked-lists-and-queues/practices/01-linked-lists/linked-list.js
@@ -1,23 +1,69 @@
+/*
+SINGLE LINK LIST
+
+add to head:
+1. length++
+2. init newNode
+
+3. set newNode.next to this.head
+4. set this.head to newNode
+
+add to tail:
+1. length++
+2. init newNode
+3. check if (this.head === null) ll is empty
+  I. YES
+     a. this.head = newNode
+     b. return
+  II. No
+     a. let current = this.head (grab the head)
+     b. while(current.next) {current = current.next}
+        (iterate to find the last)
+     c. current.next = newNode;
+        (set the (soon to be previous) last node next to newNode)
+     d. return
+
+
+*/
+
 class LinkedListNode {
   constructor(val) {
     this.value = val;
     this.next = null;
+    // this.last = null;
   }
 }
 
 class LinkedList {
   constructor() {
     this.head = null;
+    this.length = 0;
   }
 
   addToHead(val) {
-    const newNode = new LinkedListNode(value, null); // create new node
+    const newNode = new LinkedListNode(val); // create new node
     newNode.next = this.head; // set next to current head
     this.head = newNode; // set head to point to the new link
+    this.length++; // increment the length
   }
 
   addToTail(val) {
-    // Your code here
+    this.length++; // increment length
+    const newNode = new LinkedListNode(val); // create new node
+
+    if (this.head === null) {
+      // check if list empty
+      this.head = newNode; // if it is we set it to head
+      return;
+    } else {
+      let current = this.head; // grab the head
+      while (current.next) {
+        // iterate the list until end found
+        current = current.next;
+      }
+      current.next = newNode; // set the (previous) last node next to newNode
+      return;
+    }
   }
 
   // You can use this function to help debug
